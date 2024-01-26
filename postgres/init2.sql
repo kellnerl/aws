904c541c59a925c93e03acfa83548dea895b9017
tomas-holek <tomas_holek@trendmicro.com>
2024-01-26 23:52:30 +0100

    s postgres

--- postgres/init2.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE noncensuradb;
-CREATE USER postgres WITH PASSWORD  'postgres';
-ALTER ROLE postgres SET client_encoding TO 'utf8';
-ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';
-ALTER ROLE postgres SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE noncensuradb TO postgres;
