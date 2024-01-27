76abbc7041f0cb5b0eab7771c63c077fc2fbfb75
tomas-holek <tomas_holek@trendmicro.com>
2024-01-27 22:24:15 +0100

    opravy

--- dc_load_fixtures.sh
+++ dc_load_fixtures.sh
@@ -1,4 +1,6 @@
-docker-compose run web python manage.py loaddata /noncensura/diskuze/fixtures/theme_table_data.json &&
-docker-compose run web python manage.py loaddata /noncensura/diskuze/fixtures/section_table_data.json
- &&
-docker-compose run web python manage.py loaddata /noncensura/diskuze/fixtures/domain_table_data.json
+#!/bin/bash
+docker-compose run --rm web python manage.py loaddata /noncensura/diskuze/fixtures/theme_table_data.json 
+sleep 2
+docker-compose run --rm web python manage.py loaddata /noncensura/diskuze/fixtures/section_table_data.json
+sleep 2
+docker-compose run --rm web python manage.py loaddata /noncensura/diskuze/fixtures/domain_table_data.json
