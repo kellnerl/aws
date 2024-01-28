c5493eb124e52b5e6864b1fd8254008a9ff6ea02
tomas-holek <tomas_holek@trendmicro.com>
2024-01-28 05:01:11 +0100

    verze 0.63

--- diskuze/custom_context.py
+++ diskuze/custom_context.py
@@ -5,7 +5,7 @@ from datetime import date
 # custom_context.py
 def custom_data(request):
     # Zde můžete provádět logiku pro získání hodnoty, kterou chcete předat do headeru
-    version = "0.62"
+    version = "0.63"
     # Získání dnešního data
 
     response = requests.get("https://svatkyapi.cz/api/day")
